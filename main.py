991cc071050faf512645adcaba70ede455557fca
Caleb Jackson <calebjackson2002@gmail.com>
2023-12-14 22:17:43 -0800

    changed some functionality and added comments

---
+++ main.py
@@ -0,0 +1,14 @@
+from music_bot import run_bot
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+bot_token = os.getenv('DISCORD_BOT_TOKEN')
+
+def main():
+    run_bot(bot_token)
+
+if __name__ == "__main__":
+    main()
+
+    
